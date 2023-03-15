f3183f3a07b9b27690fe4d9fc2f62f5ffd118f24
Yhonimard <yhoni2103@gmail.com>
2023-03-16 04:53:23 +0700

    add

--- src/components/Cart/CartComponent.js
+++ src/components/Cart/CartComponent.js
@@ -1,10 +1,71 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
 
 const CartComponent = ({ cartData }) => {
   console.log(cartData);
   return (
-    <Container bgColor="tomato">
-      <div>testing</div>
+    <Container
+      bgColor=""
+      minH="110vh"
+      display="flex"
+      rowGap={5}
+      flexDirection="column"
+      alignItems="center"
+      mt="3"
+    >
+      <Heading flexGrow={0.5} textAlign="center">
+        CART
+      </Heading>
+      <Box flexGrow={2} bgColor="tomato" w={`full`}>
+        testingbox
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+        <div>testing</div>
+      </Box>
+      <Box flexGrow={1.5}>buying cart</Box>
     </Container>
   );
 };
