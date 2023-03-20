d6de38bfe772e125ad3ba93cf9b7d5174221339e
Yhonimard <yhoni2103@gmail.com>
2023-03-20 15:51:26 +0700

    update

--- src/lib/getCartById.js
+++ src/lib/getCartById.js
@@ -1,12 +1,12 @@
 export async function getCart() {
-  const res = await fetch("http://localhost:3000/api/cart/get");
+  const res = await fetch(`${process.env.HOST_URL}/api/cart/get`);
   const data = res.json();
 
   return data;
 }
 
 export async function getCartById(cid) {
-  const res = await fetch(`http://localhost:3000/api/cart/get/${cid}`);
+  const res = await fetch(`${process.env.HOST_URL}/api/cart/get/${cid}`);
   const data = res.json();
   return data;
 }
--- src/lib/getProductByid.js
+++ src/lib/getProductByid.js
@@ -1,12 +1,12 @@
 export async function getProduct() {
-  const res = await fetch("http://localhost:3000/api/products/get");
+  const res = await fetch(`${process.env.HOST_URL}/api/products/get`);
   const data = res.json();
 
   return data;
 }
 
 export async function getProductById(pid) {
-  const res = await fetch(`http://localhost:3000/api/products/get/${pid}`);
+  const res = await fetch(`${process.env.HOST_URL}/api/products/get/${pid}`);
   const data = res.json();
   return data;
 }
