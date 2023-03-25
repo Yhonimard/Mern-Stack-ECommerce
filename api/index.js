5d2e9b7db91f63fe7c048059a10ceac93b836ed5
Yhonimard <yhoni2103@gmail.com>
2023-03-25 21:25:35 +0700

    fixing error

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const productRouter = require("../routes/product-route");
+const userRoute = require("../routes/user-route");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use("/", (req, res, next) => {
+  res.send("hello");
+});
+
+app.use("/api/product", productRouter);
+app.use("/api/user", userRoute);
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) return next(error);
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "an unknown error occured" });
+});
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
